2f7487ce6791b7d19f7b594886588bb2323f24da
unstable <dthebestusernamelol+WHATTHESIGMA@gmail.com>
2025-02-10 18:12:22 +0800

    # photobooth

--- profiles/@a/app.js
+++ profiles/@a/app.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // QnA Link Handling
   const qnaLink = document.getElementById('qnaLink');
   qnaLink.addEventListener('click', () => {
-    window.location.href = 'https://youareadumbniggerandastupidfaggot.pages.dev/troll/gators/discord';
+    window.location.href = 'https://youareadumbniggerandastupidfaggot.pages.dev/photobooth/';
   });
 
   // Document Item Click Handling
