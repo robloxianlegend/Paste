8ba8e9c7868627236281c1b2eed7882a96fd1f04
unstable <dthebestusernamelol+WHATTHESIGMA@gmail.com>
2025-03-01 10:09:48 +0800

    # rest joiner

---
+++ rest/script.js
@@ -0,0 +1,4 @@
+document.getElementById('linkButton').addEventListener('click', function() {
+    // Replace with your desired link
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1340205856104382536'; 
+});
